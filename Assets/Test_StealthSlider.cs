0b9b93945cebe3dcf302357e91f7abf2e8705fe4
Marian Pekar <marian.pekar@gmail.com>
2019-06-09 18:55:38 +0200

    Add stealth level UI slider just for testing

---
+++ Assets/Test_StealthSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_StealthSlider : MonoBehaviour
+{
+    Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+    void Update()
+    {
+        slider.value = PlayerStates.Singleton.CurrentStealthLevel;
+    }
+}
