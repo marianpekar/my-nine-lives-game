0076a7d21dc31445251cd8a41f33b8a50a100a5e
Marian Pekar <marian.pekar@gmail.com>
2019-08-18 14:11:46 +0200

    Update DayTime script

--- Assets/World/DayTime.cs
+++ Assets/World/DayTime.cs
@@ -9,21 +9,55 @@ public class DayTime : MonoBehaviour
     [Range(0, 60)]
     public int minutes = 0;
 
+    public Color morningColor;
+    public Color noonColor;
+    public Color afternoonColor;
+    public Color nightColor;
+
+    public Color dayFog;
+    public Color nightFog;
+
+    const int MORNING = 5;
+    const int NOON = 12;
+    const int AFTERNOON = 16;
+    const int NIGHT = 19;
+
     Light sun;
     // Start is called before the first frame update
     void Start()
     {
         sun = GetComponent<Light>();
+    }
 
-        hours = Random.Range(0, 24);
-        minutes = Random.Range(0, 60);
+    private void Update()
+    {
         SetSunRotation(hours, minutes);
+        SetColor(hours);
     }
 
     // Update is called once per frame
     public void SetSunRotation(int hours, int minutes)
     {
-        float xRotation = 0.125f * (hours * 60 + minutes);
-        sun.transform.rotation = Quaternion.Euler(new Vector3(xRotation, 0,0));
+        float xRotation = 2 * (0.125f * (hours * 60 + minutes));
+        sun.transform.rotation = Quaternion.Euler(new Vector3(xRotation - 90, -30,0));
+    }
+
+    public void SetColor(int hours)
+    {
+        if (hours < MORNING || hours >= NIGHT)
+        {
+            sun.color = nightColor;
+            RenderSettings.fogColor = nightFog;
+            return;
+        }
+
+        RenderSettings.fogColor = dayFog;
+
+        if (hours >= MORNING && hours < NOON)
+            sun.color = morningColor;
+        else if (hours >= NOON && hours < AFTERNOON)
+            sun.color = noonColor;
+        else if (hours >= AFTERNOON && hours < NIGHT)
+            sun.color = afternoonColor;
     }
 }
