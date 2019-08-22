fa43d889eb525f01aedd95d5266cabdf7f911445
Marian Pekar <marian.pekar@gmail.com>
2019-08-22 11:49:30 +0200

    Modify terrain alignment to occur only when agent is moving

--- Assets/Prey/AIController.cs
+++ Assets/Prey/AIController.cs
@@ -134,8 +134,10 @@ public class AIController : MonoBehaviour
             Debug.DrawRay(this.transform.position, hit.normal, Color.magenta);
 
             Vector3 direction = agent.destination - this.transform.position;
-      
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(transform.forward + direction, hit.normal), 1.5f * Time.deltaTime);
+            
+            if(agent.speed > 0)
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(transform.forward + direction, hit.normal), 1.5f * Time.deltaTime);
+
             this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - hit.distance, this.transform.position.z);
         }
     }
