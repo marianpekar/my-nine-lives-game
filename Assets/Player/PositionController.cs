d7b36098f8bbf1ecccca082294e91904ca3d71c8
Marian Pekar <marian.pekar@gmail.com>
2019-08-25 18:01:36 +0200

    Add call for Respawn method when press F1 for testing purposes

--- Assets/Player/PositionController.cs
+++ Assets/Player/PositionController.cs
@@ -70,6 +70,10 @@ public class PositionController : MonoBehaviour
 
     public void Update()
     {
+        // TODO: Remove this before release
+        if (Input.GetKey(KeyCode.F1))
+            Respawn();
+
         //Debug.Log("Distance from spawnPoint: " + Vector3.Distance(player.transform.position, spawnPosition));
 
         float playerDistanceFromSpawnPoint = Vector3.Distance(player.transform.position, spawnPosition);
