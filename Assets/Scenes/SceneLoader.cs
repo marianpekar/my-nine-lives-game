564dd01f48caad59ea5132d0a981e685eb61cec9
Marian Pekar <marian.pekar@gmail.com>
2019-08-28 21:32:48 +0200

    Add SceneLoader and WIP main menu

---
+++ Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public int gameSceneIndex = 0;
+    public void LoadGameScene() {
+        SceneManager.LoadScene(gameSceneIndex);
+    }
+    
+}
