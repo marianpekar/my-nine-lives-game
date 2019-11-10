e05f4e2a373bb93952b3f1b80c373047e1a6e969
Marian Pekar <marian.pekar@gmail.com>
2019-11-10 17:42:01 +0100

    Add audio settings

---
+++ Assets/Options/SettingsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioMixer audioMixer;
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        audioMixer.SetFloat("sfxVolume", volume);
+    }
+}
