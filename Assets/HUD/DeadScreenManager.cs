6e0f7637509bd9a5c8e99315cef43e99a2a527ca
Marian Pekar <marian.pekar@gmail.com>
2020-01-23 23:50:36 +0100

    Add more cat facts

--- Assets/HUD/DeadScreenManager.cs
+++ Assets/HUD/DeadScreenManager.cs
@@ -12,7 +12,13 @@ public class DeadScreenManager : MonoBehaviour
     {
         "A cat’s nose is as unique as a human’s fingerprint.",
         "A cat cannot see directly under its nose.",
-        "Cats can rotate their ears 180 degrees."
+        "Cats can rotate their ears 180 degrees.",
+        "Despite popular belief, many cats are actually lactose intolerant.",
+        "Studies suggest that domesticated cats first appeared around 3600 B.C.",
+        "Cats dream, just like people do.",
+        "Meowing is a behavior that cats developed exclusively to communicate with people.",
+        "Cats live longer when they stay indoors.",
+        "Cats can jump up to six times their length."
     };
 
     private float alpha = 0f;
