a3df6e9c24c8af7379d33575c273b7493556bf6c
Marian Pekár <marian.pekar@gmail.com>
2020-04-05 23:21:51 +0200

    Add croak sound to frog prey

---
+++ Assets/NPC/Prey/PreySoundController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreySoundController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float minPlayDelay = 2f;
+    public float maxPlayDelay = 10f;
+    void Start()
+    {
+        Invoke("PlaySound", Random.Range(minPlayDelay, maxPlayDelay));
+    }
+
+    void PlaySound()
+    {
+        audioSource.Play();
+        Invoke("PlaySound", Random.Range(minPlayDelay, maxPlayDelay));
+    }
+}
