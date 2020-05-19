6e29b3c5be2200fe0bcac6b7b31e4a8ddf7d98ab
Marian Pekár <marian.pekar@gmail.com>
2020-05-19 10:27:04 +0200

    Add meow sound when life is added

---
+++ Assets/Audio/MeowManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeowManager : MonoBehaviour
+{
+    public AudioSource meow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerEvents.Singleton.RegisterLifeAddedActions(PlayMeow);
+    }
+
+    void PlayMeow()
+    {
+        meow.Play();
+    }
+}
